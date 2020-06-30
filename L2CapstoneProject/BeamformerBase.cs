fde7fbae41c7535dcf89e25ceb375c6378d27750
stchan <stephen.chan@ni.com>
2020-06-30 15:47:45 -0500

    Adding abstract base class for Beamformer

---
+++ L2CapstoneProject/BeamformerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2CapstoneProject
+{
+    abstract class BeamformerBase
+    {
+        public abstract void connect();
+        public abstract void configurePhaseAmplitudeOffset(double phase, double amp);
+        public abstract void commitPhaseAmplitudeRegister();
+        public abstract void abort();
+        public abstract void disconnect();
+    }
+}
